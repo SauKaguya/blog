301cad69c76194464ebafe8a3512353305185abe
test <email>
2024-06-11 19:07:47 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,12 @@
-const head = require('./config/head.js');
-const plugins = require('./config/plugins.js');
-const themeConfig = require('./config/themeConfig.js');
+const head = require("./config/head.js");
+const plugins = require("./config/plugins.js");
+const themeConfig = require("./config/themeConfig.js");
 
 module.exports = {
-  theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
-  description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  theme: "vdoing", // 使用npm包主题
+  title: "blog",
+  description: "vdoing博客主题模板",
+  base: "/blog/", // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
@@ -14,4 +14,23 @@ module.exports = {
   head,
   plugins,
   themeConfig,
-}
+};
+
+
+// vssue 评论插件
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "Ov23liJLknOLIfX9bRnh",
+        clientSecret: "5309d1408f85df7cdc59b7c745bd154afc3ed939",
+        owner: "saukaguya",
+        repo: "blog",
+      },
+    ],
+  ]
